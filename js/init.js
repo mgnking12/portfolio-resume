bcf3317255dcaf7de8fe1aa9b7e4e7124242391d
Megan King <mgnking12@gmail.com>
2016-07-25 16:14:15 -0500

    style

--- js/init.js
+++ js/init.js
@@ -12,6 +12,14 @@
         $('#portfolio-button').click(function() {
             $("#portfolio-section").removeClass("hide");
             $("#home-section").addClass("hide");
-        })
+        });
+        $('#back-home-p').click(function() {
+            $("#portfolio-section").addClass("hide");
+            $("#home-section").removeClass("hide");
+        });
+        $('#back-home-r').click(function() {
+            $("#resume-section").addClass("hide");
+            $("#home-section").removeClass("hide");
+        });
     }); // end of document ready
 })(jQuery); // end of jQuery name space
